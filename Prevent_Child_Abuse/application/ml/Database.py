b9660574d102dc3b1991e3e3c818478601ec174b
sig6774 <sig6774@naver.com>
2022-07-08 09:58:57 +0900

    Web

---
+++ Prevent_Child_Abuse/application/ml/Database.py
@@ -0,0 +1,80 @@
+import cx_Oracle
+import os
+import pandas as pd
+import geopy
+from geopy.geocoders import Nominatim
+from datetime import datetime, timedelta
+import geocoder
+
+os.environ["NLS_LANG"] = ".AL32UTF8"
+location_list = ['경기', '강원', '경상남도', '경상북도', '충청남도', '충청북도', '전라남도', '전라북도', '제주특별자치도', '대전', '광주', '부산', '울산', '대구', '인천']
+
+# DB내용 출력, 테스트용
+def print_db():
+    cursor.execute(sql.encode('utf-8'))
+    for row in cursor:
+        print(row)
+
+# 위치기반, 현재 날짜로부터 3~6개월전 데이터 조회
+def read_database():
+    #DB 연결
+    conn = cx_Oracle.connect("Jason/ssis1234@localhost:49161/xe")
+    # cursor = conn.cursor()
+
+    # 위치정보
+    myloc = geocoder.ip('me')
+    locator = Nominatim(user_agent = "myGeocoder")
+    coordinates = "%s, %s" %(myloc.latlng[0], myloc.latlng[1])
+    print(coordinates)
+    location = locator.reverse(coordinates)
+    my_location = None
+    
+    try:
+        my_location = location.raw['address']['city']
+    except(KeyError):
+        pass
+    try:
+        my_location = location.raw['address']['province']
+    except(KeyError):
+        pass
+
+    for i in range(len(location_list)):
+        if my_location != location_list[i]:
+            if (myloc.latlng[0] >= 37.413294 and myloc.latlng[0] <= 37.715133) and (myloc.latlng[1] >= 126.734086 and myloc.latlng[1] <= 127.269311):
+                my_location = '서울'
+            elif(myloc.latlng[0] >= 36.418608 and myloc.latlng[0] <= 36.733585) and (myloc.latlng[1] >= 127.126739 and myloc.latlng[1] <= 127.409310):
+                my_location = '세종'    
+    if my_location == '경기도':
+        my_location = '경기'
+    elif my_location == '경상남도':
+        my_location = '경남'
+    elif my_location == '경상북도':
+        my_location = '경북'
+    elif my_location == '충청남도':
+        my_location = '충남'
+    elif my_location == '충청북도':
+        my_location = '충북'
+    elif my_location == '전라남도':
+        my_location = '전남'
+    elif my_location == '전라북도':
+        my_location = '전북'
+    elif my_location == '제주특별자치도':
+        my_location = '제주'
+
+    sql_read = """select * from child_model where (신대_통계거점 = '%s' and ROWNUM < 16) order by 신고_접수일시 ASC""" % my_location
+
+    db_data = pd.read_sql(sql_read, conn)
+    conn.close()
+
+    return db_data
+
+def read_IDdata(temp_ID):
+        # DB 연결
+    conn = cx_Oracle.connect("Jason/ssis1234@localhost:49161/xe")
+
+    sql_read = """select * from child_model where (피해아동대상자 = '%s')""" % temp_ID
+
+    db_ID_data = pd.read_sql(sql_read, conn)
+    conn.close()
+
+    return db_ID_data
---
+++ Prevent_Child_Abuse/application/ml/NLP.py
@@ -0,0 +1,221 @@
+import json
+import os
+import re
+from pprint import pprint
+import rhinoMorph
+from collections import Counter
+import pandas as pd
+import math
+
+from tensorflow.compat.v2.keras.models import model_from_json
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import joblib
+import numpy as np
+import matplotlib.pyplot as plt
+from math import pi
+
+import matplotlib
+
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import numpy as np
+import math
+import tensorflow as tf
+from tensorflow.keras import models
+from tensorflow.keras import layers
+from keras.layers import Activation, SimpleRNN
+import gensim
+from gensim import models
+from gensim.models import Word2Vec, KeyedVectors
+
+common_model_url = 'application/model/'
+stopword_url = "stopwords-ko_1.txt"
+# tokenizer_url = "tokenizer.pickle"
+# vz_tokenizer_url = "vz_tokenizer.pickle"
+
+matplotlib.rcParams['axes.unicode_minus'] = False 
+matplotlib.rcParams['font.family'] = "AppleGothic"
+
+plt.switch_backend('Agg')
+plt.style.use('ggplot')
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+max_words = 5000
+max_len = 100
+
+rn = rhinoMorph.startRhino()
+
+# 텍스트 클리닝 - 한글만 남기기
+def text_cleaning(doc):
+    
+    doc = re.sub("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]", "", doc)
+        
+    return doc
+
+# 불용어 정의
+def define_stopwords(path, encoding):
+    
+    SW = set() 
+    with open(path, encoding = encoding) as f:
+        for word in f:
+            SW.add(word)
+            
+    return SW
+
+# 모델 로딩
+def load_model(model_name, model_weight):
+    global common_model_url
+    json_file = open(common_model_url + model_name, "r") 
+    loaded_model_json = json_file.read()
+    json_file.close() 
+
+    # json파일로부터 model 로드하기 
+    loaded_model = model_from_json(loaded_model_json) 
+
+    # 로드한 model에 weight 로드하기 
+    loaded_model.load_weights(common_model_url + model_weight)
+
+    return loaded_model
+
+#불용어에서 엔터키 제거
+SW = define_stopwords(common_model_url + stopword_url, encoding = 'utf-8')
+SW.add('카레')
+
+a = []
+for i in SW:
+    a.append(i.replace('\n',''))
+SW = set(a)
+# tokenizer = joblib.load(common_model_url + tokenizer_url)
+# vz_tokenizer = joblib.load(common_model_url + vz_tokenizer_url)
+
+
+# # 분석!!!
+# def sentiment_predict(new_sentence):
+#     score = 0
+#     model_name = "NLP_model.json"
+#     model_weight = "NLP_weight.h5"
+#     model = load_model(model_name, model_weight)
+#     if new_sentence != '':
+#         new_sentence1 = text_cleaning(new_sentence)
+#         new_sentence2 = rhinoMorph.onlyMorph_list(rn,new_sentence1, pos = ['NNG', 'NNP','NP', 'VV', 'VA', 'XR', 'IC', 'MM', 'MAG', 'MAJ'], eomi = False)
+#         new_sentence3 = [word for word in new_sentence2 if not word in SW] # 불용어 제거
+#         encoded = tokenizer.texts_to_sequences([new_sentence3]) # 정수 인코딩
+#         if encoded != [[]]:
+#             pad_new = pad_sequences(encoded, maxlen = max_len) # 패딩
+#             score = float(model.predict(pad_new))
+#     return score
+
+# # 시각화분석!!!
+# def sentiment_predict_VZ(new_sentence, flag):
+#     model_name = "NLP_VZ_model.json"
+#     model_weight = "NLP_VZ_weight.h5"
+#     model = load_model(model_name, model_weight)
+#     if new_sentence != '':
+#         new_sentence1 = text_cleaning(new_sentence)
+#         new_sentence2 = rhinoMorph.onlyMorph_list(rn,new_sentence1, pos = ['NNG', 'NNP','NP', 'VV', 'VA', 'XR', 'IC', 'MM', 'MAG', 'MAJ'], eomi = False)
+#         new_sentence3 = [word for word in new_sentence2 if not word in SW] # 불용어 제거
+#         encoded = vz_tokenizer.texts_to_sequences(new_sentence3)
+#         sum_list = [sum(encoded, [])]
+#         if sum_list != [[]]:
+#             padded = pad_sequences(sum_list, maxlen = max_len, value = 0, padding = 'pre')
+#             #pad_sequence에서 문제
+#             tokens = padded
+#             #     val = np.array([np.array(tokens)])
+#             pred_probs = model.predict(tokens) # 예측
+            
+#             categories = ['정서학대', '신체학대', '방임', '성학대']
+
+#             N = len(categories)
+
+#             values = np.round(pred_probs, 3).flatten().tolist()
+#             values += values[:1]
+
+#             angles = [n / float(N) * 2 * pi for n in range(N)]
+#             angles += angles[:1]
+
+#             plt.polar(angles, values)
+#             plt.fill(angles, values)
+#             plt.xticks(angles[:-1], categories)
+
+#             plt.yticks([0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1.0], color = 'black', size = 8)
+
+#             # plt.show()
+#             if flag == 1:
+#                 strFile = 'static/images/NLP_VZ_Report_results.png'
+#             else:
+#                 strFile = 'static/images/NLP_VZ_Diary_results.png'
+#             if os.path.isfile(strFile):
+#                 os.remove(strFile)   # Opt.: os.system("rm "+strFile)
+#                 print("done!")
+#             plt.savefig(strFile)
+#             plt.close()
+
+# embedding model 인코딩
+def encode_sentence_lstm(tokens, emb_size, embedding_model):
+    global common_model_url
+    embeddings = Word2Vec.load(common_model_url + embedding_model)
+    vec = np.zeros((80, 200))
+    #maxlen = 80이고 컬럼이 200?
+    for i, word in enumerate(tokens):
+        if i > 79:
+            break
+        try:
+            vec[i] = embeddings.wv[word].reshape((1, emb_size))
+        except KeyError:
+            continue
+    return vec
+
+# embedding model 분석!!!!
+def sentiment_predict_EM(new_sentence):
+    score = 0
+    model_name = "model-2.json"
+    model_weight = "model-2.h5"
+    model = load_model(model_name, model_weight)
+    if new_sentence != '':
+        new_sentence1 = text_cleaning(new_sentence)
+        new_sentence2 = rhinoMorph.onlyMorph_list(rn,new_sentence1, pos = ['NNG', 'NNP','NP', 'VV', 'VA', 'XR', 'IC', 'MM', 'MAG', 'MAJ'], eomi = False)
+        new_sentence3 = [word for word in new_sentence2 if not word in SW] # 불용어 제거\
+        if new_sentence3 != []:
+            X = np.array([encode_sentence_lstm(new_sentence3, 200, "Embedding_crawling10000.model")])
+            score = float(model.predict(X)) # 예측
+    return score
+
+# embedding model visualization
+def sentiment_predict_EM_VZ(new_sentence, flag):
+    model_name = "Multi_model.json"
+    model_weight = "Multi_weight.h5"
+    model = load_model(model_name, model_weight)
+    if new_sentence != '':
+        new_sentence1 = text_cleaning(new_sentence)
+        new_sentence2 = rhinoMorph.onlyMorph_list(rn,new_sentence1, pos = ['NNG', 'NNP','NP', 'VV', 'VA', 'XR', 'IC', 'MM', 'MAG', 'MAJ'], eomi = False)
+        new_sentence3 = [word for word in new_sentence2 if not word in SW] # 불용어 제거
+        if new_sentence3 != []:
+            X = np.array([encode_sentence_lstm(new_sentence3, 200, "Multi_Embedding_crawling10000.model")])
+            pred_probs = model.predict(X) # 예측
+            
+            categories = ['정서학대', '신체학대', '방임', '성학대']
+
+            N = len(categories)
+
+            values = np.round(pred_probs, 3).flatten().tolist()
+            values += values[:1]
+
+            angles = [n / float(N) * 2 * pi for n in range(N)]
+            angles += angles[:1]
+
+            plt.polar(angles, values)
+            plt.fill(angles, values, alpha = 0.4)
+            plt.xticks(angles[:-1], categories)
+
+            plt.yticks([0, 0.5, 1.0])
+            plt.ylim(0, 1)
+
+            if flag == 1:
+                strFile = 'static/images/NLP_VZ_Report_results.png'
+            else:
+                strFile = 'static/images/NLP_VZ_Diary_results.png'
+            if os.path.isfile(strFile):
+                os.remove(strFile)   # Opt.: os.system("rm "+strFile)
+                print("done!")
+            plt.savefig(strFile)
+            plt.close()
